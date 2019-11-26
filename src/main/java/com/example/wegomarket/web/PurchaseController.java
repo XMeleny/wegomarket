2a83bdc92213b33b532fd096f8e353aa3d99ed21
Meleny <865622793@qq.com>
2019-11-26 18:04:36 +0800

    ensure the shopping chart is not null before making purchase

--- src/main/java/com/example/wegomarket/web/PurchaseController.java
+++ src/main/java/com/example/wegomarket/web/PurchaseController.java
@@ -32,70 +32,68 @@ public class PurchaseController {
     @RequestMapping("/addPurchase")
     public String addPurchase(@RequestParam("shoppingChartIds") List<String> shoppingChartIds, RedirectAttributes redirectAttributes)
     {
-        if(shoppingChartIds!=null)
+        //before requesting, shoppingChartIds is not null
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String purchaseTime=formatter.format(new Date());
+
+        double total=0;
+        long userId=0;
+
+        List<String> productIds=new ArrayList<>();
+        List<String> productAmounts=new ArrayList<>();
+
+        long productId;
+        int productAmount;
+        int productStock;
+        double productPrice;
+
+        boolean canBuy=true;
+
+        for(String item :shoppingChartIds)
         {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-            String purchaseTime=formatter.format(new Date());
+            ShoppingChart shoppingChart=shoppingChartService.getShoppingChartById(Integer.parseInt(item));
 
-            double total=0;
-            long userId=0;
+            userId=shoppingChart.getUserId();
 
-            List<String> productIds=new ArrayList<>();
-            List<String> productAmounts=new ArrayList<>();
-
-            long productId;
-            int productAmount;
-            int productStock;
-            double productPrice;
-
-            boolean canBuy=true;
-
-            for(String item :shoppingChartIds)
+            productId=shoppingChart.getProductId();
+            productAmount=shoppingChart.getAmount();
+            productPrice=productService.findProductById(productId).getPrice();
+            productStock=productService.findProductById(productId).getStock();
+            //check the stock
+            if (productAmount>productStock)
             {
-                ShoppingChart shoppingChart=shoppingChartService.getShoppingChartById(Integer.parseInt(item));
-
-                userId=shoppingChart.getUserId();
-
-                productId=shoppingChart.getProductId();
-                productAmount=shoppingChart.getAmount();
-                productPrice=productService.findProductById(productId).getPrice();
-                productStock=productService.findProductById(productId).getStock();
-                //check the stock
-                if (productAmount>productStock)
-                {
-                    canBuy=false;
-                    break;
-                }
-
-                productIds.add(String.valueOf(productId));
-                productAmounts.add(String.valueOf(productAmount));
-                total += productAmount * productPrice;
+                canBuy=false;
+                break;
             }
 
-            if(canBuy) {
-                String productIdList = StringUtils.join(productIds, ',');
-                String productAmountList = StringUtils.join(productAmounts, ',');
-
-                Purchase purchase = new Purchase();
-                purchase.setUserId(userId);
-                purchase.setProductIdList(productIdList);
-                purchase.setProductAmountList(productAmountList);
-                purchase.setTotal(total);
-                purchase.setPurchaseTime(purchaseTime);
-
-                purchaseService.save(purchase);
-
-                //delete record in shoppingChart
-                for(String item :shoppingChartIds){
-                    shoppingChartService.delete(Long.parseLong(item));
-                }
-            }
-            //todo :else alert the stock is not enough
-
-            redirectAttributes.addAttribute("userId",userId);
-            return "redirect:/productListForUser";
+            productIds.add(String.valueOf(productId));
+            productAmounts.add(String.valueOf(productAmount));
+            total += productAmount * productPrice;
         }
 
-        return "redirect:/productList";
+        if(canBuy) {
+            String productIdList = StringUtils.join(productIds, ',');
+            String productAmountList = StringUtils.join(productAmounts, ',');
+
+            Purchase purchase = new Purchase();
+            purchase.setUserId(userId);
+            purchase.setProductIdList(productIdList);
+            purchase.setProductAmountList(productAmountList);
+            purchase.setTotal(total);
+            purchase.setPurchaseTime(purchaseTime);
+
+            purchaseService.save(purchase);
+
+            //delete record in shoppingChart
+            for(String item :shoppingChartIds){
+                shoppingChartService.delete(Long.parseLong(item));
+            }
+        }
+        //todo :else alert the stock is not enough
+
+        redirectAttributes.addAttribute("userId",userId);
+        return "redirect:/productListForUser";
+
     }
 }
